2b3768af8f4040c3ad816dec9d30231f05ee5f19
benwilcock <benwilcock@gmail.com>
2019-10-15 14:57:33 +0100

    Kicked off the project by generating a Spring Boot 'Web' project at start.spring.io with zero modifications

---
+++ src/main/java/com/benwilcock/axoninitializr/AxonInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.benwilcock.axoninitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonInitializrApplication.class, args);
+	}
+
+}
